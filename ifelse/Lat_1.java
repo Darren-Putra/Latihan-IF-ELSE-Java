5d73e329f1e8afce02b4aa0889bfb1c26bf1ed50
Darren-Putra <darren.misyantono05@gmail.com>
2024-08-21 11:27:51 +0800

    Benerin input angka nol Lat_1.java

--- ifelse/Lat_1.java
+++ ifelse/Lat_1.java
@@ -1,22 +1,28 @@
 package pabass.ifelse;
+
 import java.util.Scanner;
 
 public class Lat_1 {
-    public static void main(String[] args)
-  {
-    System.out.println("Program Untuk Cek Bilangan Ganjil/Genap");
-    System.out.println("=======================================");
+    public static void main(String[] args) {
+        System.out.println("Program Untuk Cek Bilangan Ganjil/Genap");
+        System.out.println("=======================================");
 
-    @SuppressWarnings("resource")
-    Scanner inputValue = new Scanner(System.in);
+        @SuppressWarnings("resource")
+        Scanner inputValue = new Scanner(System.in);
 
-    System.out.print("Masukkan bilangan: ");
-    int angka = inputValue.nextInt();
+        System.out.print("Masukkan bilangan: ");
+        int angka = inputValue.nextInt();
 
-    if (angka % 2 == 0) {
-      System.out.println(angka + " adalah bilangan genap");
-    } else {
-      System.out.println(angka + " adalah bilangan ganjil");
+        if (angka != 0) {
+            if (angka % 2 == 0) {
+                System.out.println(angka + " adalah bilangan genap");
+            } else {
+                System.out.println(angka + " adalah bilangan ganjil");
+            }
+
+        }
+        if (angka == 0) {
+            System.out.println(angka + " adalah bilangan Netral/Nol");
+        }
     }
-  }
 }
